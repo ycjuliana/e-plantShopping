601b626e9452e3482a4ce247c4a2f516f10db743
ycjuliana <ycjuliana@gmail.com>
2025-10-31 15:32:15 -0400

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -288,7 +288,7 @@ function ProductList({ onHomeClick }) {
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
   <div key={index}> {/* Unique key for each category div */}
     <h1>
-      <div>{category.category}</div> {/* Display the category name */}
+      <div className="center">{category.category}</div> {/* Display the category name */}
     </h1>
     <div className="product-list"> {/* Container for the list of plant cards */}
       {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
@@ -301,7 +301,7 @@ function ProductList({ onHomeClick }) {
           <div className="product-title">{plant.name}</div> {/* Display plant name */}
           {/* Display other plant details like description and cost */}
           <div className="product-description">{plant.description}</div> {/* Display plant description */}
-          <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+          <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
           <button
             className="product-button"
             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
